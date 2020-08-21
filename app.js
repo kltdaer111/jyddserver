6d288ea037bfcd83a210c354896c98c93b3e408a
major <kltdaer@gmail.com>
2020-08-21 20:11:35 +0800

    调试回调加密中

--- app.js
+++ app.js
@@ -20,6 +20,45 @@ app.use(cookieParser());
 
 HttpUtils = new httpReq(config.oapiHost);
 
+//"use strict";
+const crypto = require("crypto-js");
+
+//封装使用AES加密的方法
+// function aesEncrept(data, key, iv){
+// 　　//实例化一个cipher加密对象，使用aes192进行加密，key作为密钥
+// 　　const cipher = crypto.createCipher("aes192",key);
+// 　　//使用cipher对data进行加密，源数据类型为utf-8，输出数据类型为hex
+// 　　let crypted = cipher.update(data, "utf-8", "hex");
+// 　　crypted += cipher.final("hex");
+// 　　return crypted;
+// }
+
+//封装对应的AES解密方法
+function aesDecrept(dataStr, encodingKey) {
+    console.log('length：' + encodingKey.length);
+    var raw_key = crypto.enc.Base64.parse(encodingKey);
+    var key = raw_key.toString(crypto.enc.Utf8);
+    console.log('key:' + key);
+    var iv = key.slice(0, 16);
+    const decipher = crypto.AES.decrypt(dataStr, key,{
+        mode: crypto.mode.CBC,
+        padding: crypto.pad.Pkcs7,
+        iv: crypto.enc.Utf8.parse(iv),
+    });
+
+    const res = crypto.enc.Utf8.stringify(decipher).toString();
+    return res;
+
+    // var decipher = crypto.createDecipher("aes-128-cbc", key, iv);
+    // var cipherChunks = [];
+
+    // decipher.setAutoPadding(true);
+    // cipherChunks.push(decipher.update(dataStr, 'base64', 'utf8'));
+    // cipherChunks.push(decipher.final('utf8'));
+    // return cipherChunks.join('');
+}
+
+
 //获取token
 function getToken(func){
     HttpUtils.get("/gettoken", {
@@ -35,29 +74,45 @@ function getToken(func){
     })
 }
 
+//解码
+function dddecode(str, encoding_aeskey){
+    encoding_aeskey += '=';
+    // console.log('encoding length:' + encoding_aeskey.length);
+    // var aes_key = new Buffer(encoding_aeskey, 'base64').toString();
+    // console.log('aes_key:' + aes_key + ' ' + aes_key.length)
+    // iv = aes_key.slice(0, 16);
+    // console.log('str:' + str);
+    return aesDecrept(str, encoding_aeskey);
+}
+
 function regCallback(){
     getToken(function(accessToken){
-        HttpUtils.post("/call_back/register_call_back?access_token=" + accessToken, {
+        HttpUtils.post("/call_back/register_call_back", {
+            'access_token' : accessToken
+        }, {
             'call_back_tag': ['attendance_check_record'],
             'token': 'abcdef',
-            'aes_key': 'abcdefghijklmnopqrstuvwxyz12345678912345678',
+            'aes_key': 'xxxxxxxxlvdhntotr3x9qhlbytb18zyz5zxxxxxxxxx',
             'url': 'http://218.90.4.230:3000/callback',
         }, function(err, body){
             if(err){
                 console.error('注册回调失败');
             }else{
-				console.log('注册回调成功');
+				console.log('注册回调成功:' + body);
 			}
         });
     });
 }
 
-app.use('/callback', function(err, body){
-    if (!err) {
-        console.log('get cb res success:');
-    }else{
-        console.error('cb err');
-    }
+app.use('/callback', function(req, res){
+    console.log(req.body);
+    var data = dddecode(req.body.encrypt, 'xxxxxxxxlvdhntotr3x9qhlbytb18zyz5zxxxxxxxxx');
+    console.log('answer:' + data.length + ' ' + data);
+    // if (!err) {
+    //     console.log('get cb res success:');
+    // }else{
+    //     console.error('cb err');
+    // }
 });
 
 regCallback();
